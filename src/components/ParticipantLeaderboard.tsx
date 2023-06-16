c6d87ebd9c9381a73146739a85c25ea7e5489319
Jackson Otto <jacksonotto120@gmail.com>
2023-06-16 15:43:20 -0700

    dont show admins on leaderboard

--- src/components/ParticipantLeaderboard.tsx
+++ src/components/ParticipantLeaderboard.tsx
@@ -2,13 +2,26 @@ import { api } from "~/utils/api";
 
 const ParticipantLeaderboard = () => {
   const participants = api.participants.getParticipants.useQuery();
+  const admins = api.user.getAdmins.useQuery();
+
+  const getFilteredParticipants = () => {
+    if (!participants.data || !admins.data) return [];
+
+    const adminEmails = admins.data.map((item) => item.email);
+
+    return participants.data.filter(
+      (item) => !adminEmails.includes(item.email)
+    );
+  };
+
+  const filteredData = getFilteredParticipants();
 
   return (
     <div className="flex h-fit min-w-[350px] flex-col gap-6 rounded-md p-6 shadow-md shadow-gray-300">
       <h1 className="text-xl">Admin Emails</h1>
       <div className="flex flex-col gap-2">
-        {participants.data ? (
-          participants.data.map((participant, index) => (
+        {filteredData.length > 0 ? (
+          filteredData.map((participant, index) => (
             <div key={`participant-${index}`} className="flex gap-4">
               <div className="h-[24px] w-[24px] rounded-md bg-gray-100 text-center">
                 {index + 1}
